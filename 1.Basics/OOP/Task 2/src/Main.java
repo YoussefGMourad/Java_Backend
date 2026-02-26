ea2e20eead170c530056d0e8f56e2cd9ca165a1f
Youssef Galal <Youssef.Galal@hotmail.com>
2026-02-26 23:31:26 +0200

    Problem 4 add

--- 1.Basics/OOP/Task 2/src/Main.java
+++ 1.Basics/OOP/Task 2/src/Main.java
@@ -3,72 +3,60 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-       // Player player= new Player();
+        Player player= new Player();
 
          Scanner input = new Scanner(System.in);
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
         //        problem number 2 V 2.1
 
-//        System.out.println("Please enter the player name :");
-//        player.setPlayerName(input.next());
-//        System.out.println("Please enter the player number :");
-//        player.setPlayerNumber(input.nextInt());
-//
-//        player.printPlayerInfo();
+        System.out.println("Please enter the player name :");
+        player.setPlayerName(input.next());
+        System.out.println("Please enter the player number :");
+        player.setPlayerNumber(input.nextInt());
 
-       // System.out.println("_____________________________________________________");
+        player.printPlayerInfo();
 
-//    problem number 2 V 2.2
+        System.out.println("_____________________________________________________");
 
-//        Teacher teacher = new Teacher();
-//        System.out.println("Please enter the teacher name : ");
-//        teacher.setName(input.next());
-//        System.out.println("Please enter the teacher id :");
-//        teacher.setId(input.nextLong());
-//        System.out.println("Please enter the teacher phone number :");
-//        teacher.setPhoneNumber(input.next());
-//        System.out.println("Please enter the teacher age :");
-//        teacher.setAge(input.nextFloat());
-//        System.out.println("Please enter the teacher Salary :");
-//        teacher.setSalary(input.nextFloat());
-//
-//        System.out.println("_____________________________________________________");
-//
-//        teacher.printTeacherInfo();
+    //problem number 2 V 2.2
+
+        Teacher teacher = new Teacher();
+        System.out.println("Please enter the teacher name : ");
+        teacher.setName(input.next());
+        System.out.println("Please enter the teacher id :");
+        teacher.setId(input.nextLong());
+        System.out.println("Please enter the teacher phone number :");
+        teacher.setPhoneNumber(input.next());
+        System.out.println("Please enter the teacher age :");
+        teacher.setAge(input.nextFloat());
+        System.out.println("Please enter the teacher Salary :");
+        teacher.setSalary(input.nextFloat());
+
+        System.out.println("_____________________________________________________");
+
+        teacher.printTeacherInfo();
 
         // Problem number 3
-//        System.out.println("_____________________________________________________");
-//
-//        Calculation equ = new Calculation();
-//        System.out.println("Enter the first number :");
-//        equ.setNumberone(input.nextFloat());
-//
-//        System.out.println("Enter the second number :");
-//        equ.setNumbertwo(input.nextFloat());
-//
-//        System.out.println("Enter the third number :");
-//        equ.setNumberthree(input.nextFloat());
-//
-//        System.out.println("_____________________________________________________");
-//        equ.printTheSum();
-//
-//
-//        System.out.println("_____________________________________________________");
+        System.out.println("_____________________________________________________");
+
+        Calculation equ = new Calculation();
+        System.out.println("Enter the first number :");
+        equ.setNumberone(input.nextFloat());
+
+        System.out.println("Enter the second number :");
+        equ.setNumbertwo(input.nextFloat());
+
+        System.out.println("Enter the third number :");
+        equ.setNumberthree(input.nextFloat());
+
+        System.out.println("_____________________________________________________");
+        equ.printTheSum();
+
+
+        System.out.println("_____________________________________________________");
 
 
 
