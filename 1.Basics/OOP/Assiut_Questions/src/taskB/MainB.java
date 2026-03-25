6c606eeaa9405b9c3d4f1c2c0d02e4edea7d3400
Youssef Galal <Youssef.Galal@hotmail.com>
2026-03-25 09:36:34 +0200

    added new tasks

--- 1.Basics/OOP/Assiut_Questions/src/taskB/MainB.java
+++ 1.Basics/OOP/Assiut_Questions/src/taskB/MainB.java
@@ -1,7 +1,22 @@
 package taskB;
 
+import java.util.Scanner;
+
 public class MainB {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter a number");
+        int number = input.nextInt() ;
+        System.out.println("-------");
+        if(number<=0 ||number>1000 ) {
+            System.out.println("-1");
+        }else if (number == 1 ) {
+            System.out.println("-1");
+        }else
+        {
+            for(int i = 2  ;i <= number;i+=2){
+                System.out.println(i);
+            }
+        }
     }
 }
