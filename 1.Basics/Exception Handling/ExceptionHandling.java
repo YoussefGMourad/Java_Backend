5c31f2d27949cbc9d1aebc69a1bc9f1adfa5950c
Youssef Galal <Youssef.Galal@hotmail.com>
2026-04-01 12:04:15 +0200

    Edit some file

--- 1.Basics/Exception Handling/ExceptionHandling.java
+++ 1.Basics/Exception Handling/ExceptionHandling.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ExceptionHandling {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        try {
+            System.out.println("Please enter two numbers ");
+            int num1 = input.nextInt();
+            int num2 = input.nextInt();
+            double result = (double) num1 / num2;
+            System.out.println("The result is "+result);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero. " + e.getMessage());
 
+        } finally {
+
+            System.out.println("Done. Cleanup happens here.");
+        }
     }
 }
