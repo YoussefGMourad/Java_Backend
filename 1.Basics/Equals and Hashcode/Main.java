8a2e4fccd1799b5a7eb497ec9a105b206c52d160
Youssef Galal <Youssef.Galal@hotmail.com>
2026-04-06 09:53:55 +0200

    Some updates

--- 1.Basics/Equals and Hashcode/Main.java
+++ 1.Basics/Equals and Hashcode/Main.java
@@ -2,13 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
         Person p1 = new Person (1,"JOE");
+
         Person p2 = new Person (1,"JOE");
+
         System.out.println(p1.toString());
         System.out.println(p2.toString());
-        if(p1.equals(p2)) {
+
+        // System.out.println(p1.equals(p2));
+
+        if(p1.equals(p2))  {                 // true
             System.out.println("Equal values");
-        } else{
+        } else{                              // false
             System.out.println("Not equal values");
         }
+
+        // before overriding the Equals method the two objects aren't equal because it uses references
+        // but after overriding the Equals method the two objects are equal in values
     }
 }
--- 1.Basics/Equals and Hashcode/Person.java
+++ 1.Basics/Equals and Hashcode/Person.java
@@ -6,9 +6,8 @@ public class Person {
 
 
    Person(int id ,String name){
-
-       this.id = id;
        this.name = name;
+       this.id = id;
    };
 
     @Override
