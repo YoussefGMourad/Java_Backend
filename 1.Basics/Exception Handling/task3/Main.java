52da1ffdb1a776232d045d08d5070cc2f8af3c9a
Youssef Galal <Youssef.Galal@hotmail.com>
2026-04-07 19:19:58 +0200

    edit task3

--- 1.Basics/Exception Handling/task3/Main.java
+++ 1.Basics/Exception Handling/task3/Main.java
@@ -7,7 +7,6 @@ public class Main {
     }
     public static void main(String[] args) {
         System.out.println(printUpperCase("hello"));
-        String text =null;
         try {
 
             System.out.println(printUpperCase(null));
