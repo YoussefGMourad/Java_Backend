078a82f76fe28dd001d7b3d010550429568b9496
Youssef Galal <Youssef.Galal@hotmail.com>
2026-04-07 19:18:01 +0200

    upload task3

---
+++ 1.Basics/Exception Handling/task3/Main.java
@@ -0,0 +1,18 @@
+package task3;
+public class Main {
+
+    public static String printUpperCase(String text){
+        String upperCaseWord= text.toUpperCase();
+        return upperCaseWord;
+    }
+    public static void main(String[] args) {
+        System.out.println(printUpperCase("hello"));
+        String text =null;
+        try {
+
+            System.out.println(printUpperCase(null));
+        }catch( NullPointerException e){
+            System.out.println("Error: text is null " + e.getMessage());
+        }
+    }
+}
