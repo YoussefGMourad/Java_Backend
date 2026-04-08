f7f46b84515142215fe72e48ecf873213ebfd31b
Youssef Galal <Youssef.Galal@hotmail.com>
2026-04-08 14:55:43 +0200

    Some updates

--- 1.Basics/Equals and Hashcode/task2/Person2.java
+++ 1.Basics/Equals and Hashcode/task2/Person2.java
@@ -21,16 +21,52 @@ public class Person2 {
     }
 
 
+    // based on the Id only
+    // the output
+
+    //The size of the set is 6
+    //[Person{name='Yousef', id=1}, Person{name='Ahmed', id=2}, Person{name='Eslam', id=4},
+    // Person{name='Micheal', id=6}, Person{name='Joe', id=8}, Person{name='Ali', id=9}]
+
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Person2 person2 = (Person2) o;
+//        return id == person2.id;
+//        //&& Objects.equals(name, person2.name)
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id); //name
+//    }
+
+
+
+
+    // based on the name only
+    // the output
+
+//    The size of the set is 10
+//    [Person{name='Ahmed', id=2}, Person{name='Yousef', id=1},
+//    Person{name='Galal', id=1},Person{name='Mina', id=1},
+//    Person{name='Eslam', id=4},Person{name='Ziad', id=1}
+//    ,Person{name='Omar', id=1},Person{name='Ali', id=9}
+//    ,Person{name='Joe', id=8},  Person{name='Micheal', id=6}]
+
+
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Person2 person2 = (Person2) o;
-        return id == person2.id;
-        //&& Objects.equals(name, person2.name)
+        return  Objects.equals(name, person2.name);
+        //id == person2.id &&
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id); //name
+        return Objects.hash(name); //,id
     }
 }
